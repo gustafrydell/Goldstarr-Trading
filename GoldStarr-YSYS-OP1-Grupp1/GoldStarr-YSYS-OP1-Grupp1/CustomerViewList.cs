d736ab3a1f3ace5da6727da61d52683f4d5b9cba
Gustaf Rydell Stationära <gustaf.rydell@hotmail.com>
2020-11-02 23:35:01 +0100

    Added customerview with a list

--- GoldStarr-YSYS-OP1-Grupp1/GoldStarr-YSYS-OP1-Grupp1/CustomerViewList.cs
+++ GoldStarr-YSYS-OP1-Grupp1/GoldStarr-YSYS-OP1-Grupp1/CustomerViewList.cs
@@ -1,30 +1,36 @@
 ﻿using System.Collections.Generic;
+using Windows.ApplicationModel;
 
 namespace GoldStarr_YSYS_OP1_Grupp1
 {
     public class CustomerViewList
     {
-        public static List<Customer> GetCustomers()
+
+        public static List<Customer> Customers { get; set; } = new List<Customer>();
+
+        public CustomerViewList()
         {
-            var customers = new List<Customer>();
-
-            customers.Add(new Customer { Name = "Gustaf Larsson", Address = "Malmögatan 69", PhonenNumber = 123123123 });
-            customers.Add(new Customer { Name = "CalleBalle", Address = "Malmögatan 69", PhonenNumber = 123123123 });
-            customers.Add(new Customer { Name = "Knatte", Address = "Malmögatan 69", PhonenNumber = 123123123 });
-            customers.Add(new Customer { Name = "Fnatte", Address = "Malmögatan 69", PhonenNumber = 123123123 });
-            customers.Add(new Customer { Name = "TjackLasse", Address = "Malmögatan 69", PhonenNumber = 123123123 });
-
-
-
-            return customers;
+            Customers = GetCustomers();
         }
 
-        //public void ShowList()
-        //{
-        //    foreach (var c in )
-        //    {
 
-        //    }
-        //}
+        public static List<Customer> GetCustomers()
+        {
+            var list = new List<Customer>();
+
+            list.Add(new Customer { Name = "Gustaf Larsson", Address = "Malmögatan 69", PhoneNumber = "0706504929" });
+            list.Add(new Customer { Name = "Calle Ballsson", Address = "Drottningatan 42", PhoneNumber = "040495939" });
+            list.Add(new Customer { Name = "Yahya Yahyasson", Address = "Karstorpsvägen 32", PhoneNumber = "070492010" });
+            list.Add(new Customer { Name = "Samy kinkysson", Address = "Smultrongatan 54", PhoneNumber = "073346420" });
+            list.Add(new Customer { Name = "Alex Grottman", Address = "Kasgatan 4", PhoneNumber = "075305001" });
+
+            return list;
+        }
+
+
+        public static void AddNewUser(string name, string address, string phonenumber)
+        {
+            Customers.Add(new Customer { Name = name, Address = address, PhoneNumber = phonenumber });
+        }
     }
 }
