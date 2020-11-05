66e9ff764012b3dc05166dcb89aae0eb635c3096
Jenny Wong <jennywong567@gmail.com>
2020-11-05 17:02:21 +0100

    Added a constant string to customerViewList class

--- GoldStarr-YSYS-OP1-Grupp1/GoldStarr-YSYS-OP1-Grupp1/CustomerViewList.cs
+++ GoldStarr-YSYS-OP1-Grupp1/GoldStarr-YSYS-OP1-Grupp1/CustomerViewList.cs
@@ -6,7 +6,7 @@ namespace GoldStarr_YSYS_OP1_Grupp1
 {
     public class CustomerViewList
     {
-
+        const string notAvailable = "Ej tillgänglig";
         public static ObservableCollection<Customer> Customers { get; set; } = new ObservableCollection<Customer>();
 
         public CustomerViewList()
@@ -19,12 +19,12 @@ namespace GoldStarr_YSYS_OP1_Grupp1
         {
             var list = new ObservableCollection<Customer>();
 
-            list.Add(new Customer { Name = "Gustaf Larsson", Address = "Malmögatan 69", PhoneNumber = "0706504929", DeliveryAddress = "ej tillgänglig", CreditCardNumber = "ej tillgänglig", CustomerEmail = "ej tillgänglig" });
-            list.Add(new Customer { Name = "Calle Ballsson", Address = "Drottningatan 42", PhoneNumber = "040495939", DeliveryAddress = "ej tillgänglig", CreditCardNumber = "ej tillgänglig", CustomerEmail = "ej tillgänglig" });
-            list.Add(new Customer { Name = "Yahya Yahyasson", Address = "Karstorpsvägen 32", PhoneNumber = "070492010", DeliveryAddress = "ej tillgänglig", CreditCardNumber = "ej tillgänglig", CustomerEmail = "ej tillgänglig" });
-            list.Add(new Customer { Name = "Samy Kinkysson", Address = "Smultrongatan 54", PhoneNumber = "073346420", DeliveryAddress = "ej tillgänglig", CreditCardNumber = "ej tillgänglig", CustomerEmail = "ej tillgänglig" });
-            list.Add(new Customer { Name = "Alex Grottman", Address = "Kasgatan 4", PhoneNumber = "075305001", DeliveryAddress = "ej tillgänglig", CreditCardNumber = "ej tillgänglig", CustomerEmail = "ej tillgänglig" });
-            list.Add(new Customer { Name = "Jenny King", Address = "Skolgatan 41", PhoneNumber = "075233210", DeliveryAddress = "ej tillgänglig", CreditCardNumber = "ej tillgänglig", CustomerEmail = "ej tillgänglig" });
+            list.Add(new Customer { Name = "Gustaf Larsson", Address = "Malmögatan 69", PhoneNumber = "0706504929", DeliveryAddress = notAvailable, CreditCardNumber = notAvailable, CustomerEmail = notAvailable });
+            list.Add(new Customer { Name = "Calle Ballsson", Address = "Drottningatan 42", PhoneNumber = "040495939", DeliveryAddress = notAvailable, CreditCardNumber = notAvailable, CustomerEmail = notAvailable });
+            list.Add(new Customer { Name = "Yahya Yahyasson", Address = "Karstorpsvägen 32", PhoneNumber = "070492010", DeliveryAddress = notAvailable, CreditCardNumber = notAvailable, CustomerEmail = notAvailable });
+            list.Add(new Customer { Name = "Samy Kinkysson", Address = "Smultrongatan 54", PhoneNumber = "073346420", DeliveryAddress = notAvailable, CreditCardNumber = notAvailable, CustomerEmail = notAvailable });
+            list.Add(new Customer { Name = "Alex Grottman", Address = "Kasgatan 4", PhoneNumber = "075305001", DeliveryAddress = notAvailable, CreditCardNumber = notAvailable, CustomerEmail = notAvailable });
+            list.Add(new Customer { Name = "Jenny King", Address = "Skolgatan 41", PhoneNumber = "075233210", DeliveryAddress = notAvailable, CreditCardNumber = notAvailable, CustomerEmail = notAvailable });
 
 
             return list;
@@ -33,7 +33,7 @@ namespace GoldStarr_YSYS_OP1_Grupp1
 
         public static void AddNewStoreUser(string name, string address, string phonenumber,CustomerType customerType)
         {
-            Customers.Add(new Customer { Name = name, Address = address, PhoneNumber = phonenumber , IsOnline = customerType, DeliveryAddress = "ej tillgänglig", CreditCardNumber = "ej tillgänglig", CustomerEmail = "ej tillgänglig" });
+            Customers.Add(new Customer { Name = name, Address = address, PhoneNumber = phonenumber , IsOnline = customerType, DeliveryAddress = notAvailable, CreditCardNumber = notAvailable, CustomerEmail = notAvailable });
         }
         
         public static void AddNewOnlineUser(string name, string address, string phonenumber, CustomerType customerType, string deliveryAdress, string creditCardNumber,string customerEmail)
