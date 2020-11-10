4608cdc5c5337d5f372e3a125ef7b184e7d0e9dc
Alexander Hammargren <alex.hammargren91@gmail.com>
2020-11-10 12:41:44 +0100

    La till saknad kod i app

--- GoldStarr-YSYS-OP1-Grupp1/GoldStarr-YSYS-OP1-Grupp1/App.xaml.cs
+++ GoldStarr-YSYS-OP1-Grupp1/GoldStarr-YSYS-OP1-Grupp1/App.xaml.cs
@@ -27,6 +27,7 @@ namespace GoldStarr_YSYS_OP1_Grupp1
         public static MerchandiseManager _merchandiseManager;
         public static MerchandiseView Merchandise;
         public static CustomerOrder currentOrder;
+        public SupplierViewList SupplierList;
         public static ObservableCollection<CustomerOrder> customerOrders { get; set; } = new ObservableCollection<CustomerOrder>();
        
         /// <summary>
@@ -45,7 +46,7 @@ namespace GoldStarr_YSYS_OP1_Grupp1
             Restock = new RestockOption();
             //Merchandise = new MerchandiseView();
 
-            
+            SupplierList = new SupplierViewList();
         }
 
         /// <summary>
